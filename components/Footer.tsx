d4ad0580ca33355c696ae39cfbb6f16ac3b24558
kodeline <linekode@gmail.com>
2024-07-13 12:34:44 -0300

    Cambio en Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
       <div className="my-[3%] border-solid py-[2%] border-t-[1px] border-gray-300 text-xl text-center">
-        <p>©2024 - Kodev. derechos reservados.</p>
+        <p>© 2024 Kodev. Todos los derechos reservados.</p>
       </div>
     )
   }
